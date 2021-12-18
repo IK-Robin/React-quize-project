6df2711327f5501b829b61d8ff5eb5fe8aef6edc
Robin <62151722+IK-Robin@users.noreply.github.com>
2021-12-18 17:24:24 +0600

    push

--- src/component/App.js
+++ src/component/App.js
@@ -1,5 +1,12 @@
+import Good from './Git';
+
 const App = function () {
-    return <div>hhhh</div>;
+    return (
+        <div>
+            <h2>jjj</h2>
+            <Good />
+        </div>
+    );
 };
 
 export default App;
